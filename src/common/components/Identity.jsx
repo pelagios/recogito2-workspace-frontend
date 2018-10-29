600d8f47a51d0d851deef2fe18601b8c1c75d086
Rainer Simon <rainer.simon@ait.ac.at>
2018-10-29 16:34:00 +0100

    Minor edit

---
+++ src/common/components/Identity.jsx
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+
+export default class Identity extends Component {
+
+  // https://medium.com/@pppped/compute-an-arbitrary-color-for-user-avatar-starting-from-his-username-with-javascript-cd0675943b66
+  stringToHslColor(str) {
+    let hash = 0;
+    for (let i=0; i<str.length; i++) {
+      hash = str.charCodeAt(i) + ((hash << 5) - hash);
+    }
+    return hash % 360;
+  }
+
+  render() {
+    const dataAvailable = 
+      this.props.account && 
+      this.props.account.username &&
+      this.props.account.member_since;
+      
+    const avatarColor = (dataAvailable) ?
+     `hsl(${this.stringToHslColor(this.props.account.username)}, 35%, 65%)` : '#e2e2e2';
+
+    return (
+      <div className="identity">
+        <div className="avatar" style={{ backgroundColor: avatarColor }}>
+          <div className="inner">
+            { dataAvailable &&
+              this.props.account.username.charAt(0).toUpperCase() }
+          </div>
+        </div>
+
+        <h1>
+          { dataAvailable ?
+              ((this.props.account.real_name) ? 
+                this.props.account.real_name : this.props.account.username) :
+
+              (<span className="placeholder" />) }
+        </h1>
+
+        <p className="member-since">
+          { dataAvailable ?
+            <React.Fragment>
+              Joined on { new Intl.DateTimeFormat('en-GB', {
+                year : 'numeric',
+                month: 'short',
+                day  : '2-digit'
+              }).format(new Date(this.props.account.member_since)) }
+            </React.Fragment> : <span className="placeholder" />
+          }
+        </p>
+
+        { dataAvailable && this.props.account.bio &&
+          <p className="bio">{this.props.account.bio}</p> }
+
+        { dataAvailable && this.props.account.website && 
+          <p className="homepage">
+            <a href={this.props.account.website}>{this.props.account.website}</a>
+          </p>
+        }
+      </div>
+    )
+  }
+
+}
--- src/profile/sidebar/Sidebar.jsx
+++ src/profile/sidebar/Sidebar.jsx
@@ -1,23 +1,13 @@
 import React, { Component } from 'react';
 
+import Identity from '../../common/components/Identity.jsx';
+
 export default class Sidebar extends Component {
 
   render() {
     return (
       <div className="sidebar">
-        <div className="identity">
-          <div className="avatar">
-            <div className="inner">R</div>
-          </div>
-          <h1>Rainer Simon</h1>
-          <p className="member-since">Joined on 08 Jun 2016</p>
-        </div>
-        <div className="bio">
-          A collector of things worth knowing and things not worth knowing.
-        </div>
-        <div className="homepage">
-          <a href="http://github.com/rsimon">github.com/rsimon</a>
-        </div>
+        <Identity account={this.props.account} />
       </div>
     );
   }
