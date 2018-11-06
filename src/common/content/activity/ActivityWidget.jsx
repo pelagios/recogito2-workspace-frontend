373f5b36e5e2d88b4eaa1d740ff49f1e9e83828c
Rainer Simon <rainer.simon@ait.ac.at>
2018-11-06 14:17:13 +0100

    Refactoring/cleanup

---
+++ src/common/content/activity/ActivityWidget.jsx
@@ -0,0 +1,112 @@
+import React, { Component } from 'react';
+import { Bar, BarChart, CartesianGrid, XAxis } from 'recharts';
+import NumberFormat from 'react-number-format';
+import { ENGINE_METHOD_DIGESTS } from 'constants';
+
+const LINE_STYLE = {
+  stroke: '#c2c2c2', 
+  strokeWidth:1
+}
+
+const AXIS_STYLE = {
+  fontSize:"12px",
+  color:"#3f3f3f"
+}
+
+const WEEK = 3600000 * 24 * 7; // A week in milliseconds
+
+const tickFormatter = (tick) => new Intl.DateTimeFormat('en-GB', {
+  month: 'short'
+}).format(new Date(tick));
+
+export default class ActivityWidget extends Component {
+
+  /** Helper that generates an empty time series of N buckets with value 0 */
+  createEmptySeries(n, toTimestamp) {
+    const empty = new Array(n);
+    empty.fill(0); // Needs to contain *something* for .reduce to work
+
+    const timestamps = empty.reduce((arr, _) => {
+      const last = arr[arr.length - 1];
+      arr.push(last - WEEK);
+      return arr;
+    }, [ toTimestamp - WEEK ]).reverse();
+
+    return timestamps.map(timestamp => {
+      return { timestamp: timestamp, value: 0 };
+    });
+  }
+
+  padTimeseries(t) {
+    const toTime = t.length > 0 ? t[0] : new Date().getTime();
+    const padding = this.createEmptySeries(19 - t.length, toTime);
+    return padding.concat(t);
+  }
+
+  computeChart() {
+    // Data comes as ISO strings - convert
+    const timeseries = this.props.stats.over_time.map(t => {
+      return { timestamp: Date.parse(t.date), value: t.value };
+    });
+
+    if (timeseries.length < 20) {
+      return this.padTimeseries(timeseries);
+    } else {
+      return timeseries;
+    }
+  }
+
+  render() {
+    return (
+      <div className={this.props.className ? 
+        `activity-widget ${this.props.className}` : "activity-widget"}>
+
+        <h2>Activity 
+          {this.props.stats && 
+            <span className="count">
+              <NumberFormat 
+                displayType="text"
+                value={this.props.stats.total_contributions}
+                thousandSeparator={true} /> edits
+            </span>
+          }
+        </h2>
+
+        {this.props.stats &&
+          <BarChart 
+            width={this.props.width} 
+            height={this.props.height} 
+            barCategoryGap={1.5}
+            data={this.computeChart()} >
+
+            <CartesianGrid 
+              strokeDasharray="3" 
+              horizontal={false}/>
+
+            <XAxis 
+              type="number"
+              dataKey="timestamp"
+
+              axisLine={LINE_STYLE} 
+
+              tick={AXIS_STYLE}
+              tickSize={4}
+              tickCount={6}
+              tickLine={LINE_STYLE}
+              tickFormatter={tickFormatter}
+
+              height={20}
+              domain={['dataMin', 'dataMax']}
+              padding={{ left: 5, right: 5 }} />
+
+            <Bar 
+              dataKey="value" 
+              fill={this.props.fill} />
+
+          </BarChart>
+        }
+      </div>
+    )
+  }
+
+}
--- src/profile/sidebar/Sidebar.jsx
+++ src/profile/sidebar/Sidebar.jsx
@@ -1,96 +1,24 @@
 import React, { Component } from 'react';
-import { Bar, BarChart, XAxis, CartesianGrid } from 'recharts';
-import NumberFormat from 'react-number-format';
 
+import ActivityWidget from '../../common/content/activity/ActivityWidget.jsx';
 import CollaboratorList from '../../common/content/profile/CollaboratorList.jsx';
 import ProfileInfo from '../../common/content/profile/ProfileInfo.jsx';
 
 export default class Sidebar extends Component {
 
-  createEmpty(num, toTimestamp) {
-    const interval = 3600000 * 24 * 7; // 1 week
-
-    const empty = new Array(num);
-    empty.fill(0);
-
-    const timestamps = empty.reduce((arr, _) => {
-      const prev = arr[arr.length - 1];
-      arr.push(prev - interval);
-      return arr;
-    }, [ toTimestamp - interval ]).reverse();
-
-    return timestamps.map(timestamp => {
-      return { timestamp: timestamp, value: 0 };
-    });
-  }
-
-  padEdits(edits) {
-    if (edits.length < 20) {
-      const lastStamp = (edits.length > 0) ? edits[0].timestamp : new Date().getTime();
-      const head = this.createEmpty(19 - edits.length, lastStamp);
-      return head.concat(edits); 
-    }
-
-    return edits;
-  }
-
   render() {
-    const edits = (this.props.account) ? this.props.account.stats.over_time.map(obj => {
-      return { timestamp: Date.parse(obj.date), value: obj.value };
-    }) : null;
-
-    const tickFormatter = (tick) => new Intl.DateTimeFormat('en-GB', {
-      month: 'short'
-    }).format(new Date(tick));
-    
     return (
       <div className="sidebar">
         <div className="section">
           <ProfileInfo account={this.props.account} />
         </div>
 
-        <div className="section compact edit-stats">
-          <h2>Activity 
-            {this.props.account && 
-              <span className="count">
-                <NumberFormat 
-                  displayType="text"
-                  value={this.props.account.stats.total_contributions}
-                  thousandSeparator={true} /> edits
-              </span>
-            }
-          </h2>
-          {this.props.account && 
-            <BarChart width={237} height={55} data={this.padEdits(edits)} barCategoryGap={1.5}>
-              <CartesianGrid strokeDasharray="3" horizontal={false}/>
-              <XAxis 
-                dataKey="timestamp"
-                type="number"
-                tick={{
-                  fontSize:"12px",
-                  color:"#3f3f3f",
-                  
-                  
-                }}
-                tickLine={{
-                  stroke: '#c2c2c2', 
-                  strokeWidth:1
-                }}
-                axisLine={{ 
-                  stroke: '#c2c2c2',
-                  strokeWidth:1
-                }}
-                tickSize={4}
-                tickCount={6}
-                height={20}
-                tickFormatter={tickFormatter}
-                domain={['dataMin', 'dataMax']}
-                padding={{ left: 5, right: 5 }}
-                 />
-              <Bar dataKey="value" fill="#4483c4" />
-            </BarChart>
-          }
-        </div>
+        <ActivityWidget 
+          className="section" 
+          width={237}
+          height={55}s
+          fill="#4483c4"
+          stats={this.props.account && this.props.account.stats} />
 
         <CollaboratorList 
           className="section"
