4fcadc50279ba107f83529e00f97d6dd371ad650
Rainer Simon <rainer.simon@ait.ac.at>
2018-11-07 12:03:22 +0100

    Small deploy helper shell script

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+# A simple helper to copy the contents of the build folder to Recogito.
+# Use at your own risk!
+echo "Removing JS and view templates from previous build"
+rm -rf ../recogito2/public/javascripts/static/
+rm ../recogito2/app/views/my/profile.scala.html
+rm ../recogito2/app/views/my/workspace.scala.html
+
+echo "Copying files from /build folder"
+cp -a ./build/static/ ../recogito2/public/javascripts/.
+cp ./build/profile.scala.html ../recogito2/app/views/my/.
+cp ./build/workspace.scala.html ../recogito2/app/views/my/.
