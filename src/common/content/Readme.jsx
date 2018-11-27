2f41ae7c830ea5a3630f75215669de8aa4f65001
Rainer Simon <rainer.simon@ait.ac.at>
2018-11-27 20:39:14 +0100

    Readme: edit mode styling

--- src/common/content/Readme.jsx
+++ src/common/content/Readme.jsx
@@ -3,12 +3,23 @@ import ReactMarkdown from 'react-markdown';
 
 export default class Readme extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = { editing: false }
+  }
+
   render() {
     return (
-      <div className="readme">
-        <div className="inner">
-          <ReactMarkdown source={this.props.children} />
-        </div>
+      <div className={this.state.editing ? "readme editing" : "readme"}>
+       {this.state.editing ? 
+          <div 
+            className="inner"
+            contentEditable={true}>{this.props.children}</div>
+          :
+          <div className="inner">
+            <ReactMarkdown source={this.props.children} />            
+          </div>
+        }
       </div>
     )
   }
