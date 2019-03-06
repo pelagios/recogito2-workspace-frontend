b5070fd103b6feb11741c609ea70b67ef4e7ec02
Rainer Simon <rainer.simon@ait.ac.at>
2019-03-06 11:06:47 +0100

    Local state plumbing for permissions change + remove collaborator

--- src/common/content/share/collaborators/Collaborators.jsx
+++ src/common/content/share/collaborators/Collaborators.jsx
@@ -7,12 +7,29 @@ export default class Collaborators extends Component {
     collaborators: [
       { username: 'rainer', access_level: 'WRITE' },
       { username: 'elton', access_level: 'READ'}
-    ]
+    ],
   }
 
-  openPermissionsSelector = (evt) => {
+  openPermissionsSelector = (evt, collaborator) => {
     const button = evt.target.nodeName === 'BUTTON' ? evt.target : evt.target.parentNode;
-    openSelector(button.parentNode);
+    openSelector({
+      parent: button.parentNode,
+      accessLevel: collaborator.access_level,
+      onChangeLevel: level => this.setState(prev => {
+        return {
+          collaborators: prev.collaborators.map(c => {
+            return (c.username === collaborator.username) ?
+              { username: c.username, access_level: level } : c;
+          })
+        }
+      })
+    });
+  }
+
+  removeCollaborator = (collaborator) => {
+    this.setState(prev => {
+      return { collaborators: prev.collaborators.filter(c => c.username !== collaborator.username) }
+    });
   }
 
   render() {
@@ -22,13 +39,15 @@ export default class Collaborators extends Component {
         <td>
           <button
             className="btn small permissions"
-            onClick={this.openPermissionsSelector}>
+            onClick={(evt) => this.openPermissionsSelector(evt, c)}>
             <span className="label">{c.access_level}</span>
             <span className="icon">{'\uf0dd'}</span>
           </button>
         </td>
-        <td className="col-actions outline-icon remove-collaborator">
-          {'\ue897'}
+        <td className="col-actions remove-collaborator">
+          <button 
+            className="nostyle outline-icon"
+            onClick={() => this.removeCollaborator(c)}>{'\ue897'}</button>
         </td>
       </tr>
     );
--- src/common/content/share/collaborators/PermissionSelector.js
+++ src/common/content/share/collaborators/PermissionSelector.js
@@ -3,36 +3,75 @@ import { render} from 'react-dom';
 
 class PermissionSelector extends Component {
 
+  componentDidMount() {
+    document.addEventListener('mousedown', this.onMousedown, false);
+    document.addEventListener('keydown', this.onKeydown, false);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('mousedown', this.onMousedown, false);
+    document.removeEventListener('keydown', this.onKeydown, false);
+  }
+
+  onMousedown = (evt) => {
+    const isClickOutside = !this._node.contains(evt.target);
+    if (isClickOutside) this.props.onCancel();
+  }
+
+  onKeydown = (evt) => {
+    evt.stopPropagation();
+    if (evt.which === 27) this.props.onCancel();
+  }
+
   handleCancel= () => {
     this.props.onCancel();
   }
 
+  changeLevel(level) {
+    this.props.onChangeLevel(level);
+  }
+
   render() {
     return (
-      <div className="permission-selector">
+      <div 
+        className="permission-selector"
+        ref={n => this._node = n}>
+
         <h3>Permission Level
           <span className="close outline-icon" onClick={this.handleCancel}>{'\ue897'}</span>
         </h3>
         <ul>
-          <li className="READ">
+          <li
+            className="READ"
+            onClick={() => this.changeLevel('READ')}>
             <div className="checked">
-              <div className="icon" />
+              <div className="icon">
+                {this.props.accessLevel === 'READ' && '\uf00c' }
+              </div>
             </div>
             <h4>Read</h4>
             <p>Collaborators can read document and annotations, but not edit.</p>
           </li>
 
-          <li className="WRITE">
+          <li 
+            className="WRITE"
+            onClick={() => this.changeLevel('WRITE')}>
             <div className="checked">
-              <div className="icon" />
+              <div className="icon">
+                {this.props.accessLevel === 'WRITE' && '\uf00c' }
+              </div>
             </div>
             <h4>Write</h4>
             <p>Collaborators can read document and annotations, create new annotations, add and reply to comments.</p>
           </li>
 
-          <li className="ADMIN">
+          <li
+            className="ADMIN"
+            onClick={() => this.changeLevel('ADMIN')}>
             <div className="checked">
-              <div className="icon" />
+              <div className="icon">
+                {this.props.accessLevel === 'ADMIN' && '\uf00c' }
+              </div>
             </div>
             <h4>Admin</h4>
             <p>Admins can edit document metadata, invite other collaborators, backup and restore, and roll back the edit history.</p>
@@ -44,16 +83,24 @@ class PermissionSelector extends Component {
 
 }
 
-export function openSelector(parentEl) {
+export function openSelector(config) {
+  const parent = config.parent;
   const root = document.createElement('div');
-  parentEl.append(root);
+  parent.append(root);
 
   const onCancel = () => {
     root.remove(); 
   }
 
+  const onChangeLevel = (level) => {
+    config.onChangeLevel(level);
+    root.remove();
+  }
+
   render(
     <PermissionSelector 
+      accessLevel={config.accessLevel}
+      onChangeLevel={onChangeLevel}
       onCancel={onCancel} />, 
   root);
 }
