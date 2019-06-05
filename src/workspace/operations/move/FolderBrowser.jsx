2eb695241515fc18d9f432e59af9e7d53a4d09ba
Rainer Simon <rainer.simon@ait.ac.at>
2019-06-05 11:54:22 +0200

    Prepartory plumbing: animated folder transition

--- src/workspace/operations/move/FolderBrowser.jsx
+++ src/workspace/operations/move/FolderBrowser.jsx
@@ -1,29 +1,51 @@
 import React, { Component } from 'react';
 import Draggable from 'react-draggable';
+import ReactCSSTransitionReplace from 'react-css-transition-replace';
+
+// Just a hack
+const FOLDERS = [
+  { id: 'root', title: 'Root folder', hasSubfolders: true },
+  { id: 'level1-1', parent: 'root', title: 'Folder A', hasSubfolders: true },
+  { id: 'level1-2', parent: 'root', title: 'Folder B', hasSubfolders: false },
+  { id: 'level2-1', parent: 'level1-1', title: 'Folder C', hasSubfolders: false }
+];
 
 export default class FolderBrowser extends Component {
   
   state = {
-    currentFolder: 'Lorem ipsum',
+    currentFolder:  FOLDERS[0],
     subfolders: [
-      { id: 'foo', title: 'Folder A', hasSubfolders: true },
-      { id: 'bar', title: 'Folder B', hasSubfolders: false }
+      FOLDERS[1], 
+      FOLDERS[2]
     ],
     selected: null
   }
 
   select = id => this.setState({ selected: id });
 
-  navigateUp = () => {
+  goToFolder = id => {
+    const currentFolder = FOLDERS.find(f => f.id === id);
+    const subfolders = FOLDERS.filter(f => f.parent === id);
+    this.setState({ currentFolder, subfolders });
+  }
 
+  navigateUp = () => {
+    // TODO just a hack
+    if (this.state.currentFolder.parent)    
+      this.goToFolder(this.state.currentFolder.parent);
+  }
+
+  navigateInto = folderId => {
+    this.goToFolder(folderId);
   }
 
   render() {
     const folders = this.state.subfolders.map(f => 
       <li 
-        className={this.state.selected === f.id && 'selected'}
+        key={f.id}
+        className={this.state.selected === f.id ? 'selected' : undefined}
         onClick={() => this.select(f.id)}>{f.title}
-        { f.hasSubfolders && <button className="nostyle icon">&#xe684;</button> }
+        { f.hasSubfolders && <button className="nostyle icon" onClick={() => this.navigateInto(f.id)}>&#xe684;</button> }
       </li>
     );
 
@@ -37,13 +59,20 @@ export default class FolderBrowser extends Component {
                   className="nostyle icon"
                   onClick={this.navigateUp}>&#xe686;</button>
 
+                <span className="title">{this.state.currentFolder.title}</span>
+
                 <button
                   className="nostyle icon cancel"
                   onClick={this.props.onCancel}>&#xe680;</button>
               </div>
 
               <div className="body">
-                <ul>{folders}</ul>
+                <ReactCSSTransitionReplace
+                  transitionName="wipe"
+                  transitionEnterTimeout={200}
+                  transitionLeaveTimeout={200}>
+                  <ul key={this.state.currentFolder.id}>{folders}</ul>
+                </ReactCSSTransitionReplace>
               </div>
 
               <div className="footer">
