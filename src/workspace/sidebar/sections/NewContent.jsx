28aea0fa65200c3eec6d9e1f259a962245cb1242
Rainer Simon <aboutgeo@gmail.com>
2021-01-14 13:30:41 +0100

    Cleanup

--- src/workspace/sidebar/sections/NewContent.jsx
+++ src/workspace/sidebar/sections/NewContent.jsx
@@ -91,7 +91,7 @@ export default class NewContent extends Component {
               { this.hasFeature('webmaps') && <Menu.Item
                 icon={'\uf278'}
                 label="Web Map (WMTS, XYZ)"
-                onSelect={this.select(() => this.props.onImportSource('WMTS'))} />
+                onSelect={this.select(() => this.props.onImportSource('WMTS_XYZ'))} />
               }
             </Menu.Group>
           </Menu>
